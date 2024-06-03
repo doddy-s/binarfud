94826e7cbd4d94dc6487f723dc2e0d0a13a5b6d5
doddy-s <doddy.s@students.amikom.ac.id>
2024-06-03 20:39:06 +0700

    chore: set up project

---
+++ src/main/java/com/doddysujatmiko/binarfud/BinarFudApplication.java
@@ -0,0 +1,13 @@
+package com.doddysujatmiko.binarfud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinarFudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinarFudApplication.class, args);
+    }
+
+}
